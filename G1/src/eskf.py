7a1728c990e44eac07b03b80022a4943507a778d
Marie Bringsvor <marie.bringsvor@gmail.com>
2022-10-22 10:46:11 +0200

    Finished all tasks except 3b

--- G1/src/eskf.py
+++ G1/src/eskf.py
@@ -12,6 +12,7 @@ from states import NominalState, ErrorState
 
 from quaternion import RotationQuaterion
 from utils.cross_matrix import get_cross_matrix
+from utils.indexing import block_3x3
 from sensors import SensorGNSS
 from models import ModelIMU
 from solution import eskf as eskf_solu
@@ -37,22 +38,15 @@ class ESKF():
             x_est_pred: predicted eskf state
         """
         if dt == 0:
-
-            # TODO remove this
-            x_est_prev = eskf_solu.ESKF.predict_from_imu(
-                self, x_est_prev, z_imu, dt)
             return x_est_prev
 
         x_est_prev_nom = x_est_prev.nom
-        z_corr = None  # TODO
-        x_est_pred_nom = None  # TODO
-        x_est_pred_err = None  # TODO
+        z_corr = self.model.correct_z_imu(x_est_prev_nom, z_imu)
+        x_est_pred_nom = self.model.predict_nom(x_est_prev_nom, z_corr, dt)
+        x_est_pred_err = self.model.predict_err(x_est_prev, z_corr, dt)
 
         x_est_pred = EskfState(x_est_pred_nom, x_est_pred_err)
 
-        # TODO remove this
-        x_est_pred = eskf_solu.ESKF.predict_from_imu(
-            self, x_est_prev, z_imu, dt)
         return x_est_pred
 
     def update_err_from_gnss(self,
@@ -84,21 +78,18 @@ class ESKF():
         x_err = x_est_pred.err
         z_pred, S = z_est_pred
 
-        innovation = None  # TODO
-        H = None  # TODO
-        P = None  # TODO
-        R = None  # TODO
-        W = None  # TODO
-        x_err_upd = np.zeros(15)  # TODO
-        I_WH = None  # TODO
-        x_err_cov_upd = np.eye(15)  # TODO
+        innovation = z_gnss - z_pred
+        H = self.sensor.H(x_nom)
+        P = x_err.cov
+        R = self.sensor.R
+        W = P @ H.T @ np.linalg.inv(H @ P @ H.T + R)
+        x_err_upd = W @ innovation
+        I_WH = np.eye(*P.shape) - W @ H
+        x_err_cov_upd = (I_WH @ P @ I_WH.T + W @ R @ W.T)
 
         x_err_upd = ErrorState.from_array(x_err_upd)
         x_est_upd_err = MultiVarGauss[ErrorState](x_err_upd, x_err_cov_upd)
 
-        # TODO remove this
-        x_est_upd_err = eskf_solu.ESKF.update_err_from_gnss(
-            self, x_est_pred, z_est_pred, z_gnss)
         return x_est_upd_err
 
     def inject(self,
@@ -117,21 +108,27 @@ class ESKF():
         Returns:
             x_est_inj: eskf state after injection
         """
-        pos_inj = np.zeros(3)
-        vel_inj = np.zeros(3)
-        ori_inj = RotationQuaterion(1, np.zeros(3))
-        accm_bias_inj = np.zeros(3)
-        gyro_bias_inj = np.zeros(3)
+        pos_inj = x_est_nom.pos + x_est_err.mean.pos
+        vel_inj = x_est_nom.vel + x_est_err.mean.vel
+        ori_inj = x_est_nom.ori.multiply( RotationQuaterion(1, 0.5 * x_est_err.mean.avec) )
+        accm_bias_inj = x_est_nom.accm_bias + x_est_err.mean.accm_bias
+        gyro_bias_inj = x_est_nom.gyro_bias + x_est_err.mean.gyro_bias
 
         x_nom_inj = NominalState(pos_inj, vel_inj, ori_inj,
                                  accm_bias_inj, gyro_bias_inj)
 
-        P_inj = np.eye(15)
+        G = np.zeros((15,15))
+        G[block_3x3(0,0)] = np.eye(3)
+        G[block_3x3(1,1)] = np.eye(3)
+        G[block_3x3(2,2)] = np.eye(3) - get_cross_matrix(0.5 * x_est_err.mean.avec)
+        G[block_3x3(3,3)] = np.eye(3)
+        G[block_3x3(4,4)] = np.eye(3)
+
+        P_inj = G @ x_est_err.cov @ G.T
+
         x_err_inj = MultiVarGauss[ErrorState](np.zeros(15), P_inj)
         x_est_inj = EskfState(x_nom_inj, x_err_inj)
 
-        # TODO remove this
-        x_est_inj = eskf_solu.ESKF.inject(self, x_est_nom, x_est_err)
         return x_est_inj
 
     def update_from_gnss(self,
@@ -152,12 +149,8 @@ class ESKF():
             z_est_upd: predicted measurement gaussian
 
         """
-        z_est_pred = None  # TODO
-        x_est_upd_err = None  # TODO
-        x_est_upd = None  # TODO
-
-        # TODO remove this
-        x_est_upd, z_est_pred = eskf_solu.ESKF.update_from_gnss(
-            self, x_est_pred, z_gnss)
+        z_est_pred = self.sensor.pred_from_est(x_est_pred)
+        x_est_upd_err = self.update_err_from_gnss(x_est_pred, z_est_pred, z_gnss)
+        x_est_upd = self.inject(x_est_pred.nom, x_est_upd_err)
 
         return x_est_upd, z_est_pred
--- G1/src/sensors.py
+++ G1/src/sensors.py
@@ -3,7 +3,8 @@ from typing import Optional
 import numpy as np
 
 from senfuslib import MultiVarGauss
-from states import NominalState, GnssMeasurement, EskfState
+from states import NominalState, GnssMeasurement, EskfState, ErrorState
+from quaternion import RotationQuaterion
 from utils.cross_matrix import get_cross_matrix
 from solution import sensors as sensors_solu
 
@@ -30,9 +31,13 @@ class SensorGNSS:
         Returns:
             H (ndarray[3, 15]): the measurement matrix
         """
+        R = x_nom.ori.R
+        S_a = get_cross_matrix(self.lever_arm)
 
-        # TODO remove this
-        H = sensors_solu.SensorGNSS.H(self, x_nom)
+        H = np.zeros((3,15))
+        H[0:3, 0:3] = np.eye(3)
+        H[0:3, 6:9] = -R @ S_a
+   
         return H
 
     def pred_from_est(self, x_est: EskfState,
@@ -47,12 +52,23 @@ class SensorGNSS:
         """
         x_est_nom = x_est.nom
         x_est_err = x_est.err
-        z_pred = np.zeros(3)  # TODO
-        S = np.eye(3)  # TODO
+        H = self.H(x_est_nom)
+        R = self.R
+
+        # Should we use x_t = x_nom + x_err ?
+        # x_t_pos = x_est_nom.pos + x_est_err.mean.pos
+        # x_t_vel = x_est_nom.vel + x_est_err.mean.vel
+        # x_t_ori = x_est_nom.ori.multiply(RotationQuaternion.from_avec(1, 0.5*x_est_err.mean.avec))
+        # x_t_acc = x_est_nom.accm_bias + x_est_err.mean.accm_bias
+        # x_t_gyro = x_est_nom.gyro_bias + x_est_err.mean.gyro_bias
+        # x_t = NominalState(x_t_pos, x_t_vel, x_t_ori, x_t_acc, x_t_gyro)
+
+        z_pred = H @ x_est_err.mean
+        S = H @ x_est_err.cov @ H.T + R
 
         z_pred = GnssMeasurement.from_array(z_pred)
         z_gnss_pred_gauss = MultiVarGauss[GnssMeasurement](z_pred, S)
 
-        # TODO remove this
+        # Finish task and remove this:
         z_gnss_pred_gauss = sensors_solu.SensorGNSS.pred_from_est(self, x_est)
         return z_gnss_pred_gauss
